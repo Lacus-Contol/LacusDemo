681888819131fa0cad392a04777e4d07c063e661
Oscar Loya <GameReview43@Yahoo.com>
2020-12-02 23:59:46 -0700

    Lectura de datos en BD

--- app/src/main/java/com/example/lacusdemo/AltaActivity.java
+++ app/src/main/java/com/example/lacusdemo/AltaActivity.java
@@ -7,11 +7,23 @@ import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class AltaActivity extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Camion + n"); //Añadir lectura de camion por numeracion
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alta);
 
@@ -30,8 +42,43 @@ public class AltaActivity extends AppCompatActivity {
         btnAction[0].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                for (EditText btn : txtRecive)
-                    btn.setText("");
+                for (EditText txt : txtRecive)
+                    txt.setText("");
+            }
+        });
+
+        btnAction[1].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("Camion + n"); //Añadir lectura de camion por numeracion
+
+                List<String> Truck = new ArrayList<String>();
+                for (EditText txt : txtRecive)
+                    Truck.add(txt.getText().toString());
+                Truck.add("New");
+                Truck.add("34");    //Cambiar por variables de ubicacion actual
+                Truck.add("151");
+
+                myRef.setValue(Truck);
+                for (EditText txt : txtRecive)
+                    txt.setText("");
+            }
+        });
+
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                value = "55";
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                //Log.w(TAG, "Failed to read value.", error.toException());
             }
         });
     }
--- app/src/main/java/com/example/lacusdemo/MapsActivity.java
+++ app/src/main/java/com/example/lacusdemo/MapsActivity.java
@@ -1,8 +1,8 @@
 package com.example.lacusdemo;
 
-import androidx.fragment.app.FragmentActivity;
-
 import android.os.Bundle;
+
+import androidx.fragment.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,6 +10,11 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -25,22 +30,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-
-        // Add multiple markers in Sydney and move the camera
+// Add multiple markers in Sydney and move the camera
         LatLng[] markers = {
                 new LatLng(-34, 151),
                 new LatLng(-35, 152),
@@ -50,7 +44,36 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         for (char i = 0; i < markers.length; i++)
             mMap.addMarker(new MarkerOptions().position(markers[i]).title("Marker in Sydney"));
+
         //camera on central point markers[0]
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(markers[0], 10));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference();
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                Object value = dataSnapshot.getValue(Object.class);
+                value = "5";
+                //Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                //Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+
     }
 }
